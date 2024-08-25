5125a0a2e91edef4f6ca517bb7767819e46b7e40
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-08-25 19:24:42 +1000

    Setup basic styling

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
 
 const site = lume({
     src: "./src",
 });
 
 site.use(jsx());
+site.use(lightningCss());
 
 export default site;
