9ff5cd6f277aca9fb03dc3e1e1abc6ace7162dd3
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-08-25 20:37:54 +1000

    Fix indentation

--- site/_includes/layouts/base.tsx
+++ site/_includes/layouts/base.tsx
@@ -1,17 +1,17 @@
 export default ({title, children}: Lume.Data) => {
-    return (
+  return (
     <html lang="en">
-        <head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>{title}</title>
-            <link rel="stylesheet" href="/styles.css" />
-        </head>
-        <body>
-            <main>
-                {children}
-            </main>
-        </body>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
-    );
+  );
 }
