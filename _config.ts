086266a1718459fc37ff47a84b86429115c20b23
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-11-12 19:32:56 +1100

    Update fonts

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
 import jsx from "lume/plugins/jsx.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 import mdx from "lume/plugins/mdx.ts";
@@ -9,11 +10,13 @@ const site = lume({
 });
 
 // Plugins
+site.use(googleFonts({
+    fonts: "https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Ubuntu+Mono:wght@400;700&display=swap"
+}));
 site.use(jsx());
 site.use(lightningCss());
 site.use(mdx());
 
-site.copy("assets/fonts");
 site.copy("assets/scripts");
 
 export default site;
